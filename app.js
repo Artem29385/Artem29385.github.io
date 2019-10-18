4e734f639b26500028ea6d63cc20ae974d89eca2
Artem29385 <55153434+Artem29385@users.noreply.github.com>
2019-10-18 17:26:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,79 +9,81 @@ const blockMap = document.querySelector('[animate-halfblock-map-js]');
 const header = document.querySelector('[data-js-dir]');
 
 const closeMenu = () => {
-   burger.classList.toggle('active');
-   headerBar.classList.toggle('active');
-   headerMenu.classList.toggle('active');
-   headerMenu.nextElementSibling.classList.toggle('active');
-   headerMenu.nextElementSibling.nextElementSibling.classList.toggle('active');
-   close.classList.toggle('active');
+  burger.classList.toggle('active');
+  headerBar.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  headerMenu.nextElementSibling.classList.toggle('active');
+  headerMenu.nextElementSibling.nextElementSibling.classList.toggle('active');
+  close.classList.toggle('active');
 }
-
-const heightHeader = header.offsetHeight / 1.1;
+let heightHeader = header.offsetHeight / 1.1;
+window.addEventListener('resize', () => {
+	heightHeader = header.offsetHeight / 1.1;
+});
 let value = window.pageYOffset;
 window.addEventListener('scroll', function () {
-   if ((window.pageYOffset > value + 50) && (window.pageYOffset > heightHeader)) {
-      header.setAttribute('data-js-dir', 'down')
-      headerBar.setAttribute('data-js-dir', 'down');
-      value = window.pageYOffset;
-      if (burger.className.includes('active')) {
-         closeMenu();
-      }
-   } else if (window.pageYOffset < value) {
-      header.setAttribute('data-js-dir', 'up')
-      headerBar.setAttribute('data-js-dir', 'up');
-      value = window.pageYOffset;
-   }
+  if ((window.pageYOffset > value + 50) && (window.pageYOffset > heightHeader)) {
+    header.setAttribute('data-js-dir', 'down')
+    headerBar.setAttribute('data-js-dir', 'down');
+    value = window.pageYOffset;
+    if (burger.className.includes('active')) {
+      closeMenu();
+    }
+  } else if (window.pageYOffset < value) {
+    header.setAttribute('data-js-dir', 'up')
+    headerBar.setAttribute('data-js-dir', 'up');
+    value = window.pageYOffset;
+  }
 });
 
 close.onclick = () => {
-   closeMenu();
+  closeMenu();
 };
 
 burger.onclick = () => {
-   closeMenu();
+  closeMenu();
 }
 let posTop;
 window.addEventListener('beforeunload', () => {
-   window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 });
 
 setTimeout(function () {
-   window.scrollTo(0, 1);
+  window.scrollTo(0, 1);
 }, 1);
 let flag = false;
 if (document.body.offsetWidth > 640) {
-   let halfBlock;
-   window.addEventListener('scroll', function () {
-         if (!flag) {
-            posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-            posTop = posTop + window.innerHeight - intro.getBoundingClientRect().top * 2;
-            if (posTop > intro.getBoundingClientRect().top) {
-               intro.style.opacity = 1;
-               intro.style.transform = 'translate3d(0, 0, 0)';
-            }
-            if (posTop > blockGallery.getBoundingClientRect().top) {
-               blockGallery.style.opacity = 1;
-               blockGallery.style.transform = 'translate3d(0, 0, 0)';
-            }
-            if (posTop / 2 > blockAbout.getBoundingClientRect().top) {
-               halfBlock = document.querySelector('.lays');
-               halfBlock.lastElementChild.style.transform = 'translate3d(0, 0, 0)';
-               halfBlock.firstElementChild.style.transform = 'translate3d(0, 0, 0)';
-               halfBlock.firstElementChild.style.opacity = 1;
-               halfBlock.lastElementChild.style.opacity = 1;
-            }
-            if (posTop / 2 > blockMap.getBoundingClientRect().bottom) {
-               halfBlock = blockMap;
-               halfBlock.lastElementChild.style.transform = 'translate3d(0, 0, 0)';
-               halfBlock.firstElementChild.style.transform = 'translate3d(0, 0, 0)';
-               halfBlock.firstElementChild.style.opacity = 1;
-               halfBlock.lastElementChild.style.opacity = 1;
-               flag = true;
-            }
+  let halfBlock;
+  window.addEventListener('scroll', function () {
+       if (!flag) {
+         posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+         posTop = posTop + window.innerHeight - intro.getBoundingClientRect().top * 2;
+         if (posTop > intro.getBoundingClientRect().top) {
+           intro.style.opacity = 1;
+           intro.style.transform = 'translate3d(0, 0, 0)';
          }
-      }
-   );
+         if (posTop > blockGallery.getBoundingClientRect().top) {
+           blockGallery.style.opacity = 1;
+           blockGallery.style.transform = 'translate3d(0, 0, 0)';
+         }
+         if (posTop / 2 > blockAbout.getBoundingClientRect().top) {
+           halfBlock = document.querySelector('.lays');
+           halfBlock.lastElementChild.style.transform = 'translate3d(0, 0, 0)';
+           halfBlock.firstElementChild.style.transform = 'translate3d(0, 0, 0)';
+           halfBlock.firstElementChild.style.opacity = 1;
+           halfBlock.lastElementChild.style.opacity = 1;
+         }
+         if (posTop / 2 > blockMap.getBoundingClientRect().bottom) {
+           halfBlock = blockMap;
+           halfBlock.lastElementChild.style.transform = 'translate3d(0, 0, 0)';
+           halfBlock.firstElementChild.style.transform = 'translate3d(0, 0, 0)';
+           halfBlock.firstElementChild.style.opacity = 1;
+           halfBlock.lastElementChild.style.opacity = 1;
+           flag = true;
+         }
+       }
+     }
+  );
 }
 intro.style.opacity = 1;
 document.querySelector('body').style.backgroundColor = 'white';
